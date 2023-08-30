705b07a136d2e5bba392fab815680374c0c845f1
Joao Santos <joao.santos@wnd.ch>
2023-08-30 08:12:36 +0200

    Added empty basket look

--- src/components/molecule/Header.tsx
+++ src/components/molecule/Header.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from 'react-i18next'
 import Search from '@/components/atomic/Search'
 import Logo from '@/assets/images/logo.png'
+import EmptyBasket from '@/assets/images/empty-basket.svg'
 import { ShoppingCartOutlined } from '@ant-design/icons'
 import { Button, Modal, Divider, Badge } from 'antd'
 import { JSX, useState } from 'react'
@@ -24,17 +25,24 @@ const Header = () => {
     const total = basketProducts.reduce((a, c) => a + Number(c.price) * c.amount, 0)
 
     return <div className="my-6 min-h-[5rem]">
-      <div>
-        {basketProducts.map((basketProduct: BasketProduct, i) => <div key={basketProduct.id}>
-        <span className="flex justify-between">
-          <span>{`${basketProduct.amount}x ${basketProduct.name}`}</span>
-          <span>{`${(Number(basketProduct.price) * basketProduct.amount).toFixed(2)}chf`}</span>
-        </span>
-        {i < basketProducts.length - 1 && <Divider className="my-1"/>}
+      {total > 0 ?
+        (<>
+          <div>
+          {basketProducts.map((basketProduct: BasketProduct, i) => <div key={basketProduct.id}>
+                <span className="flex justify-between">
+                    <span>{`${basketProduct.amount}x ${basketProduct.name}`}</span>
+                    <span>{`${(Number(basketProduct.price) * basketProduct.amount).toFixed(2)}chf`}</span>
+                </span>
+              {i < basketProducts.length - 1 && <Divider className="my-1"/>}
+            </div>
+          )}
         </div>
-        )}
-      </div>
-      <span className="mt-8 flex justify-between">{'Total: '}<b>{`${total.toFixed(2)}chf`}</b></span>
+        <span className="mt-8 flex justify-between">{'Total: '}<b>{`${total.toFixed(2)}chf`}</b></span>
+        </>) :
+        <div className="flex flex-col justify-center">
+          <img className="h-24" src={EmptyBasket} alt="empty basker" />
+          <span className="mx-auto mt-4 text-[1rem]">{t('basket.empty')}</span>
+        </div>}
     </div>
   }
 
@@ -65,7 +73,7 @@ const Header = () => {
       </header>
       <Modal
         open={open}
-        title="Basket"
+        title={t('basket.title')}
         onCancel={handleCancel}
         footer={[]}
       >
