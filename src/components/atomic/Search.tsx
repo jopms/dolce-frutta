df060fb6cfb1c77681b72c7d444e82d3e772af3e
Joao Santos <joao.santos@wnd.ch>
2023-08-30 15:32:36 +0200

    Update

--- src/components/atomic/Search.tsx
+++ src/components/atomic/Search.tsx
@@ -1,11 +1,13 @@
-import { Input } from 'antd'
+import { Input, Button } from 'antd'
 import { SearchOutlined } from '@ant-design/icons'
 import RestApi from '@/api/RestApi'
 import { MAX_RESULTS } from '@/helper/globals'
 import { useDispatch } from 'react-redux'
 import { setLoading, setProducts } from '@/redux/products/productsSlice'
+import { useTranslation } from 'react-i18next'
 
 const Search = (props: { placeholder: string, disabled: boolean }) => {
+  const { t } = useTranslation()
   const useRestApi = new RestApi()
   const dispatch = useDispatch()
 
@@ -26,13 +28,18 @@ const Search = (props: { placeholder: string, disabled: boolean }) => {
   }
 
   return (
-      <Input
-        prefix={<SearchOutlined />}
-        className={'rounded-3xl bg-gray-100 hover:bg-gray-100 [&>input]:ml-2'}
-        placeholder={props.placeholder}
-        bordered={false}
-        onKeyDown={onInput}
-      />
+      <>
+        <Input
+          prefix={<SearchOutlined />}
+          className={'rounded-3xl bg-gray-100 hover:bg-gray-100 [&>input]:ml-2'}
+          placeholder={props.placeholder}
+          bordered={false}
+          onKeyDown={onInput}
+        />
+        <Button>
+          {t('main.search.showAll')}
+        </Button>
+      </>
     )
 }
 
